206e09441fe211f0bf864e3293335ffcd8304100
jangky000 <jangky000@mme.dongguk.edu>
2020-09-15 18:10:07 +0900

    sytle, css loader 설치

---
+++ front/source/index.js
@@ -0,0 +1,6 @@
+// 엔트리 파일
+// 번들로 변환해서 사용
+// npx webpack --entry ./source/index.js --output ./public/index_bundle.js
+
+// config 설정 시
+// npm webpack --config webpack.config.js
---
+++ front/webpack.config.js
@@ -0,0 +1,14 @@
+// npx webpack --entry ./source/index.js --output ./public/index_bundle.js
+// npx webpack --config webpack.config.js 또는 그냥 npx webpack
+
+const path = require('path');
+module.exports = {
+    mode:'development',
+    enrty:"./source/index.js",
+    output:{
+        path: path.resolve(__dirname, "public"),
+        filename: 'index_bundle.js'
+    }
+}
+
+
